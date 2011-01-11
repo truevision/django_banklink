bed85d9c888737979e25e8a9f263099511b8c2e0
Kristaps Kulis <Kristaps.Kulis@gmail.com>
2011-01-11 10:16:42 +0200

    settings wrapper

---
+++ django_banklink/settings.py
@@ -0,0 +1,15 @@
+from django.conf import settings 
+from django.core.exceptions import ImproperlyConfigured
+
+PRIVATE_KEY =  getattr(settings, 'BANKLINK_PRIVATE_KEY', None)
+if not PRIVATE_KEY:
+    raise ImproperlyConfigured(u"BANKLINK_PRIVATE_KEY not found in settings")
+
+SND_ID = getattr(settings, 'BANKLINK_SND_ID', None)
+if not SND_ID:
+    raise ImproperlyConfigured(u"BANKLINK_SND_ID not found in settings")
+
+BANKLINK_URL = getattr(settings, 'BANKLINK_REQUEST_URL',
+                                'https://ib.swedbank.lv/banklink')
+
+
