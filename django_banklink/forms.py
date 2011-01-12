f99549597470c1bdd8b6fa33f5e4956b834a43ba
Kristaps Kulis <Kristaps.Kulis@gmail.com>
2011-01-12 15:22:08 +0200

    correct signature

--- django_banklink/forms.py
+++ django_banklink/forms.py
@@ -8,15 +8,17 @@ class PaymentRequest(forms.Form):
     VK_SND_ID = forms.CharField(widget = forms.HiddenInput())
     VK_STAMP = forms.CharField(widget = forms.HiddenInput())
     VK_AMOUNT = forms.CharField(widget = forms.HiddenInput())
+    VK_CURR = forms.CharField(widget = forms.HiddenInput())
     VK_REF = forms.CharField(widget = forms.HiddenInput())
     VK_MSG = forms.CharField(widget = forms.HiddenInput())
-    VK_MAC = forms.CharField(widget = forms.HiddenInput())
+    VK_MAC = forms.CharField(widget = forms.HiddenInput(), required = False)
     VK_RETURN = forms.CharField(widget = forms.HiddenInput())
     VK_LANG = forms.CharField(widget = forms.HiddenInput())
     VK_ENCODING = forms.CharField(widget = forms.HiddenInput())
     def __init__(self, *args, **kwargs):
         initial = {} 
         initial['VK_STAMP'] = kwargs.get('description')
+        initial['VK_MSG'] = kwargs.get('message')
         initial['VK_AMOUNT'] = kwargs.get('amount')
         initial['VK_CURR'] = kwargs.get('currency', 'LVL')
         initial['VK_REF'] = kwargs.get('reference')
@@ -27,8 +29,15 @@ class PaymentRequest(forms.Form):
         initial['VK_VERSION'] = '008'
         initial['VK_LANG'] = kwargs.get('language', 'LAT')
         initial['VK_SND_ID'] = settings.SND_ID
-        initial['VK_MAC'] = create_signature(initial)
         super(PaymentRequest, self).__init__(initial, *args)
+        if self.is_valid():
+            mac = create_signature(self.cleaned_data)
+            self.data['VK_MAC'] = mac
+            if not self.is_valid():
+                raise RuntimeError("signature is invalid")
+        else:
+            print self.errors
+            raise RuntimeError("invalid initial data")
     def as_html(self, with_submit = False, id = "banklink_payment_form"):
         html = u'<form action="%s" method="POST" id="%s">' % (settings.BANKLINK_URL, id)
         for field in self:
@@ -37,8 +46,3 @@ class PaymentRequest(forms.Form):
             html += '<input type="submit" />'
         html += '</form>'
         return html 
-
-    
-    
-    
-
--- django_banklink/utils.py
+++ django_banklink/utils.py
@@ -1,6 +1,27 @@
 from M2Crypto import EVP
 from django_banklink import settings
-from base64 import b64encode 
+
+def get_ordered_request(request):
+    return (request['VK_SERVICE'],
+            request['VK_VERSION'],
+            request['VK_SND_ID'],
+            request['VK_STAMP'],
+            request['VK_AMOUNT'],
+            request['VK_CURR'],
+            request['VK_REF'],
+            request['VK_MSG'])
+
+def request_digest(request):
+    """ 
+        return request digest in Banklink signature form (see docs for format)
+    """
+    request = get_ordered_request(request)
+    digest = r''
+    for value in request:
+        digest += str(len(value)).rjust(3, '0')
+        digest += str(value)
+    print digest
+    return digest
 
 def create_signature(request):
     """ 
@@ -8,11 +29,25 @@ def create_signature(request):
     """
 
     key = EVP.load_key(settings.PRIVATE_KEY)
-    formatted = ''
-    for val in request:
-        formatted += unicode(len(val)).ljust(3, '0')
-        formatted + val
+    digest = request_digest(request)
+    key.reset_context(md = 'sha1')
     key.sign_init()
-    key.sign_update(formatted)
+    key.sign_update(digest)
     key_binary = key.sign_final()
-    return b64encode(key_binary)
+    
+    b21 = key_binary.encode('base64').strip()
+    print b21
+    return b21 
+
+def verify_signature(request, signature):
+    """
+        verify BankLink reply signature
+    """
+    signature = unicode(signature)
+    pubkey = EVP.load_key(settings.PUBLIC_KEY)
+    pubkey.verify_init()
+    pubkey.verify_update(request_digest(request))
+    if pubkey.verify_final(signature.decode('base64')) == 1:
+        return True
+    else:
+        return False
