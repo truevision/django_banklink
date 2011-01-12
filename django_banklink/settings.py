3490d10f7831c6517991b0a0f1b9f761590d4c04
Kristaps Kulis <Kristaps.Kulis@gmail.com>
2011-01-12 16:11:42 +0200

    add settings.py

--- django_banklink/settings.py
+++ django_banklink/settings.py
@@ -9,6 +9,10 @@ SND_ID = getattr(settings, 'BANKLINK_SND_ID', None)
 if not SND_ID:
     raise ImproperlyConfigured(u"BANKLINK_SND_ID not found in settings")
 
+PUBLIC_KEY = getattr(settings, 'BANKLINK_PUBLIC_KEY', None)
+if not PUBLIC_KEY:
+    raise ImproperlyConfigured(u"BANKLINK_PUBLIC_KEY not found in settings")
+
 BANKLINK_URL = getattr(settings, 'BANKLINK_REQUEST_URL',
                                 'https://ib.swedbank.lv/banklink')
 
