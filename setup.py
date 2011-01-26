57b9993b3597cfe535d139e7a078cb7292f04bb4
Kristaps Kulis <Kristaps.Kulis@gmail.com>
2011-01-26 19:54:19 +0200

    update version  clasifiers

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name = "django-banklink",
     packages = ["django_banklink"],
-    version = "0.12",
+    version = "0.14",
     description = "django application for Swedbank payment gateway",
     author = "Kristaps Kulis",
     author_email = "kristaps@true-vision.net",
     url = "http://www.github.com/truevision/django_banklink",
     classifiers = [
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
